fde7b8510a5abe1087f12789f07411daad1574d1
rioramski <rio.ramski@nashta.co.id>
2020-03-15 22:03:18 +0700

    update logo

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 const propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 const defaultProps = {};
 
 class DefaultFooter extends Component {
   render() {
-
     // eslint-disable-next-line
     const { children, ...attributes } = this.props;
 
     return (
       <React.Fragment>
-        <span><a href="https://coreui.io">CoreUI</a> &copy; 2020 poc-INSW.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">Edii</a></span>
+        <span>
+          <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
+            CoreUI
+          </a>{" "}
+          &copy; 2020 POC INSW.
+        </span>
+        <span className="ml-auto">
+          Powered by{" "}
+          <a
+            href="https://edi-indonesia.co.id/id/about-us/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Edii
+          </a>
+        </span>
       </React.Fragment>
     );
   }
--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
 import DefaultHeaderDropdown  from './DefaultHeaderDropdown'
-import logo from '../../assets/img/brand/logo.svg'
+import logo from '../../assets/img/brand/logo.png'
 import sygnet from '../../assets/img/brand/sygnet.svg'
 
 const propTypes = {
@@ -24,8 +24,8 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+          full={{ src: logo, width: 92, height: 30, alt: 'INSW Logo' }}
+          minimized={{ src: sygnet, width: 30, height: 30, alt: 'INSW Logo' }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
         <Nav className="d-md-down-none" navbar>
